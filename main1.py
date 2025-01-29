0a82c11edd02dcbfcf6239e4f52e1dd4f31a91c1
askarbek0va <askarbekovameerim08@gmail.com>
2025-01-29 15:07:42 +0600

    second commit

--- main1.py
+++ main1.py
@@ -1,9 +1,13 @@
 from person import Person
 
 if __name__=='__main__':
-    person1=Person('Aiza', 'Sarlykova',20,"female")
+    person1 = Person('Meerim', 'Askarbekova', 19)
     person1.walk()
     person1.info()
+
+    person2=Person('Aiza', 'Sarlykova',20,"female")
+    person2.walk()
+    person2.info()
 
     a=Person('Aiturgan', 'Kanybekova',19)
     a.walk()
--- person.py
+++ person.py
@@ -12,6 +12,3 @@ class Person:
         print(f'Person {self.name} {self.surname} is {self.age} years old.')
         print('Person name =', self.name,'\n' ' surname =', self.surname,'\n' ' age =', self.age,'\n' ' gender =', self.gender )
 
-person1=Person('Meerim', 'Askarbekova',19)
-person1.walk()
-person1.info()
