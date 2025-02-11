bd4987fe8e816c7516add26834e9973d8d1675cc
askarbek0va <askarbekovameerim08@gmail.com>
2025-02-11 11:35:12 +0600

    commit

--- assignment_3/object.py
+++ assignment_3/object.py
@@ -4,7 +4,7 @@ if __name__=='__main__':
     account = PersonalAccount(int(input('Please enter your account number:')), input('Please enter your account holder:'))
     print(account)
 
-    account.deposit(float(input('\nAmount of deposit:')))
+    account.deposit(float(input('\nAmount of deposit: ')))
     account.withdraw((float(input('\nAmount of withdrawal:'))))
     account.withdraw((float(input('\nAmount of withdrawal:'))))
 
